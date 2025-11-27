ee5d84b16800a35f4d8f5b4c70cca0cc296692b2
ZachSyv <zachary@syvenky.com>
2025-11-27 04:32:21 -0800

    made the hands a bit stiffer

--- source/Juggling_Agent/Juggling_Agent/tasks/direct/juggling_agent/juggling_agent_env_cfg.py
+++ source/Juggling_Agent/Juggling_Agent/tasks/direct/juggling_agent/juggling_agent_env_cfg.py
@@ -70,13 +70,13 @@ def get_hand_cfg(prim_name, usd_file_name, pos, rot, joint_pos):
                 },
                 stiffness={
                     "WRJ.*": 5.0,
-                    "(FF|MF|RF|LF|TH)J(4|3|2|1)": 1.0,
-                    "(LF|TH)J5": 1.0,
-                    "elbow_(rotate|bend)": 1.0
+                    "(FF|MF|RF|LF|TH)J(4|3|2|1)": 5.0,
+                    "(LF|TH)J5": 5.0,
+                    "elbow_(rotate|bend)": 10.0
                 },
                 damping={
                     "WRJ.*": 0.5,
-                    "(FF|MF|RF|LF|TH)J(4|3|2|1)": 0.1,
+                    "(FF|MF|RF|LF|TH)J(4|3|2|1)": 0.5,
                     "(LF|TH)J5": 0.1,
                     "elbow_(rotate|bend)": 1.0
                 },
